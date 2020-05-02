054a21d3ef72032d9954eb3734b1b1bd9f81e826
zhennanh <hzn@xkool.info>
2020-05-02 11:24:08 +0800

    fix constant

--- constant.py
+++ constant.py
@@ -1,4 +1,4 @@
-path_head = '../Live_demo_202001171/'
+path_head = '../Live_demo_20200117/'
 
 image_path_head = path_head + 'image/'
 image_annos_path_head = path_head + 'image_annotation/'
@@ -14,8 +14,13 @@ train_json_path = path_head + "train.json"
 valid_img_dir = path_head + "video_cut"
 valid_json_path = path_head + "train.json"
 
-test_video_frame_dir = path_head + 'video_cut'
-test_image_dir = path_head + 'image'
+test_path_head = '/tcdata/test_dataset_3w/'
+test_video_path_raw = test_path_head + 'video/'
+test_video_path_head = test_path_head + 'video_cut/'
+test_image_path = test_path_head + 'image/'
+
+train_data_all_path = path_head+'/train_data_all/'
+train_data_all_path_no_pair = path_head+'/train_data_all_nopair/'
 test_video_frame_annos_path = 'video_boxes.json'
 test_image_frame_annos_path = 'image_boxes.json'
 
